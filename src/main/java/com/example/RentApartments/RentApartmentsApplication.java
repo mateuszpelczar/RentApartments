ca8c4275fba325eb2b162da5198b6dbadf7fd4e4
Mateusz Pelczar <matrix1555555@gmail.com>
2025-11-17 19:13:03 +0100

    Dodano projekt na GitHub

---
+++ src/main/java/com/example/RentApartments/RentApartmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RentApartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentApartmentsApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/example/RentApartments/RentApartmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RentApartments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApartmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
